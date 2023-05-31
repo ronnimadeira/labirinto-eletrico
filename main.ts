1b0162aa649bcae4aadb8927e9e7a0f27f1f0692
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-05-31 16:56:54 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,31 +3,32 @@ let tempo = 0
 let status = 0
 input.onPinPressed(TouchPin.P0, function () {
     Número += 1
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
     basic.showIcon(IconNames.No)
-    basic.pause(1000)
+    basic.pause(100)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
-    music.playTone(988, music.beat(BeatFraction.Whole))
+    music.playTone(880, music.beat(BeatFraction.Whole))
+    basic.showString("E")
     basic.showNumber(Número)
     basic.pause(2000)
 })
 input.onButtonPressed(Button.A, function () {
     Número = 0
     tempo = 0
-    status = 0
-    music.playTone(392, music.beat(BeatFraction.Whole))
     status = 1
+    music.playTone(988, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     music.playTone(880, music.beat(BeatFraction.Whole))
+    basic.showString("T")
     basic.showNumber(tempo)
     basic.pause(2000)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
     status = 2
 })
 basic.forever(function () {
@@ -35,7 +36,5 @@ basic.forever(function () {
     if (status == 1) {
         tempo += 1
         basic.showIcon(IconNames.SmallHeart)
-    } else if (status == 2) {
-    	
     }
 })
